0bac2ccfae4369c0451f3b70836fba4a5b68a438
zubcarz <zubcarz@gmail.com>
2017-07-10 12:07:40 -0500

    fisrt commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("!!Node Start¡¡");
+/*var storage = require("node-persist");
+
+storage.initSyns();
+
+storage.setItemSync('name','Carlos');
+
+var name = storage.getItemSync("name");
+console.log("Name Save:" + name);
+*/
